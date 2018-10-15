f1fbf21e859ffc9e8359e1c18e47a66245f428b9
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-15 14:15:35 +0200

    .

--- Ejercicios Objetos Satelite/Program.cs
+++ Ejercicios Objetos Satelite/Program.cs
@@ -10,6 +10,16 @@ namespace Ejercicios_Objetos_Satelite
     {
         static void Main(string[] args)
         {
+            bool orbita;
+            Satelite s1 = new Satelite(1000,1000,1000);
+            s1.PrintPosicion();
+            orbita = s1.EnOrbita();
+            if (orbita)
+            {
+                Console.WriteLine("Hasta el infinito y más allá");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejercicios Objetos Satelite/satelite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_Objetos_Satelite
+{
+        public class Satelite
+        {
+            private double meridiano;
+            private double paralelo;
+            private double distancia_tierra;
+
+            public Satelite(double m, double p, double d)
+            {
+                meridiano = m;
+                paralelo = p;
+                distancia_tierra = d;
+            }
+
+            public Satelite()//Constructor vacio
+            {
+                meridiano = paralelo = distancia_tierra = 0;
+            }
+
+            public void SetPosicion(double m, double p, double d)
+            {
+                meridiano = m;
+                paralelo = p;
+                distancia_tierra = d;
+            }
+
+            public void PrintPosicion()
+            {
+                Console.WriteLine("El satélite se encuentra en el paralelo " + paralelo +
+                "Meridiano"+meridiano+" a una distancia de la tierra de "+distancia_tierra+
+                "Kilómetros");
+                Console.ReadLine();
+            }
+            public void VariaAltura(double desplazamiento)
+            {
+                distancia_tierra = distancia_tierra + desplazamiento;
+            }
+            public bool EnOrbita()
+            {
+                bool orbita=false;
+                if (distancia_tierra>0)
+                {
+                    orbita=true;
+                }
+                return orbita;
+            }
+            public void VariaPosicion(double variap,double variam)
+            {
+                paralelo = paralelo + variap;
+                meridiano = meridiano + variam;
+            }
+        }
+    }
+
+//Modifica la clase satélite y añádele los siguientes métodos:
+//Método void VariaAltura(double desplazamiento): Este método acepta un parámetro que será positivo o negativo dependiendo de si el satélite tiene que alejarse o acercarse a La Tierra.
+//Método boolean EnOrbita() : Este método devolverá false si el satélite está en tierra, y true en caso contrario.
+// Método void VariaPosicion(double variap, double variam): Este método permite modificar los atributos de 
+//posición(meridiano y paralelo) mediante los parámetros variap y variam.Estos parámetros serán valores positivos o negativos 
+//relativos que harán al satélite modificar su posición.
